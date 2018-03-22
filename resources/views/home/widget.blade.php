32dfafa82b0750d850af674f626e9fd6dafa87fa
Nikita <Nkmelnikov@hotmail.com>
2018-03-22 18:29:28 +0300

    widged::naming

--- resources/views/home/widget.blade.php
+++ resources/views/home/widget.blade.php
@@ -1,12 +1,12 @@
 <section class="x-widget">
 
 <div class="widget-part x-widget__top">
-        <p>До конца ICO:</p>
+        <p class="js__stage-name-title">До конца ICO:</p>
         <div class="control-container">
             <div class="c c1">
                 <div class="x-progress c1__progress_pre">
                     <div class="x-in x-progress__in_pre"></div>
-                    <span class="x-round js__round-pre">Pre-ICO</span>
+                    <span class="x-stage js__stage-pre">Pre-ICO</span>
                     <div class="x-progress_dates">
                         <span class="dates__start_pre">20.01.18</span>
                         <span class="dates__end_pre">21.02.18</span>
@@ -14,7 +14,7 @@
                 </div>
                 <div class="x-progress c1__progress_ico">
                     <div class="x-in x-progress__in_ico"></div>
-                    <span class="x-round js__round-ico">ICO</span>
+                    <span class="x-stage js__stage-ico">ICO</span>
                     <div class="x-progress_dates">
                         <span class="dates__start_ico">20.03.18</span>
                         <span class="dates__end_ico">21.05.18</span>
@@ -23,16 +23,16 @@
             </div>
             <div class="c c2">
                 <div class="d d1">
-                    <span class="t js_d1__t">12</span>
-                    <span class="d__name">дни</span>
+                    <span class="t js__dd">12</span>
+                    <span class="d__name js__dd-name">дни</span>
                 </div>
                 <div class="d d2">
-                    <span class="t js_d2__t">18</span>
-                    <span class="d__name">часы</span>
+                    <span class="t js__hh">18</span>
+                    <span class="d__name js__hh-name">часы</span>
                 </div>
                 <div class="d d3">
-                    <span class="t js_d3__t">59</span>
-                    <span class="d__name">минуты</span>
+                    <span class="t js__mm">59</span>
+                    <span class="d__name js__mm-name">минуты</span>
                 </div>
             </div>
         </div>
@@ -46,12 +46,12 @@
                     <span class="x-progress__percents">55%</span>
                     <div class="x-progress__caps">
                         <div class="x-progress__caps_crypto">
-                            <span class="dates__start_pre">123 123 TOK</span> of
-                            <span class="dates__end_pre">9 422 000 TOK</span>
+                            <span class="js_caps__start_crypto">123 123 TOK</span> of
+                            <span class="js_caps__end_crypto">9 422 000 TOK</span>
                         </div>
                         <div class="x-progress__caps_fiat">
-                            <span class="dates__start_pre">10 000 USD</span> of
-                            <span class="dates__end_pre">50 000 000 USD</span>
+                            <span class="js_caps__start_fiat">10 000 USD</span> of
+                            <span class="js_caps__end_fiat">50 000 000 USD</span>
                         </div>
                     </div>
                 </div>
@@ -59,14 +59,14 @@
 
             <div class="c c2">
                 <div class="c2__cur">
-                    <img src="{{ asset('img/Ethereum.png') }}" alt="">
+                    <img class="c2__cur_img0" src="{{ asset('img/Ethereum.png') }}" alt="">
                     <div class="tb0 text-box">
                         <span class="a0 amount">70</span>
                         <span class="n0 name">ETH</span>
                     </div>
                 </div>
                 <div class="c2__cur">
-                    <img src="{{ asset('img/Bitcoin.png') }}" alt="">
+                    <img class="c2__cur_img1" src="{{ asset('img/Bitcoin.png') }}" alt="">
                     <div class="tb1 text-box">
                         <span class="a1 amount">46.30</span>
                         <span class="n1 name">BTC</span>
