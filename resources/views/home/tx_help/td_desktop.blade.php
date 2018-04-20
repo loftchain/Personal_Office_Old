1e25819196740be540ffc8c34ea16779c83c79a4
Nikita <Nkmelnikov@hotmail.com>
2018-04-20 11:39:50 +0300

    транзакции

--- resources/views/home/tx_help/td_desktop.blade.php
+++ resources/views/home/tx_help/td_desktop.blade.php
@@ -1,6 +1,5 @@
 <tr class="td-block">
     <td class="td-value">1</td>
-    <td class="td-from">2</td>
     <td class="td-to">3</td>
     <td class="td-status">3</td>
     <td class="td-info"><a class="td-info-link" href="">4</a></td>
