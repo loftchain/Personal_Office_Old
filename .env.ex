61364208c300f197087d75f47ad4e5f788abe1c5
Nikita <Nkmelnikov@hotmail.com>
2018-05-08 12:13:19 +0300

    all stages filled/ TODO: widget display

--- .env.ex
+++ .env.ex
@@ -49,9 +49,17 @@ VERSION=0.13
 
 UPLOAD_PATH=C:\OSPanel\domains\pao.loc\public\uploads\
 
-//----------------------------------------------------
-PRE_SALE_START=1526860800       # Monday, May 21, 2018 12:00:00 AM
-PRE_SALE_END=1528761599         # Monday, June 11, 2018 11:59:59 PM
+# ----------------------------------------------------
+
+BONUS_THRESHOLD1=1
+BONUS_THRESHOLD2=4
+BONUS_THRESHOLD3=8
+BONUS_THRESHOLD4=15
+
+# ----------------------------------------------------
+
+PRE_SALE_START=1526860800       # Monday, May 21, 2018 00:00:00
+PRE_SALE_END=1528761599         # Monday, June 11, 2018 23:59:59
 
 PRE_SALE_SOFT_CAP=250           # ETH
 PRE_SALE_HARD_CAP=770           # ETH
@@ -62,99 +70,80 @@ PRE_SALE_MIN_PAY=0.1            # ETH
 PRE_SALE_WALLET_ETH=0x950D69e56F4dFE84D0f590E0f9F1BdC6d60A46A9
 PRE_SALE_WALLET_BTC=btcbtc1
 
-PRE_SALE_THRESHOLD1=1           # ETH
 PRE_SALE_BONUS1=15              # %
-PRE_SALE_THRESHOLD2=4           # ETH
 PRE_SALE_BONUS2=20              # %
-PRE_SALE_THRESHOLD3=8           # ETH
 PRE_SALE_BONUS3=25              # %
-PRE_SALE_THRESHOLD4=15          # ETH
 PRE_SALE_BONUS4=30              # %
-//----------------------------------------------------
-PRE_ICO_START=
-PRE_ICO_END=
 
-PRE_ICO_SOFT_CAP=1526860800
-PRE_ICO_HARD_CAP=
+# ----------------------------------------------------
 
-PRE_ICO_TOKEN_PRICE=
-PRE_ICO_MIN_PAY=
+PRE_ICO_START=1528761600        # Tuesday, 12 June 2018 г., 00:00:00
+PRE_ICO_END=1530662399          # Tuesday, 3 July 2018 г., 23:59:59
 
-PRE_ICO_WALLET_ETH=
-PRE_ICO_WALLET_BTC=
+PRE_ICO_SOFT_CAP=770            # ETH
+PRE_ICO_HARD_CAP=1230           # ETH
 
-PRE_ICO_THRESHOLD1=
-PRE_ICO_BONUS1=
-PRE_ICO_THRESHOLD2=
-PRE_ICO_BONUS2=
-PRE_ICO_THRESHOLD3=
-PRE_ICO_BONUS3=
-PRE_ICO_THRESHOLD4=
-PRE_ICO_BONUS4
-//----------------------------------------------------
-ICO1_START=
-ICO1_END=
+PRE_ICO_TOKEN_PRICE=13000       # LDX
+PRE_ICO_MIN_PAY=0.1             # ETH
 
-ICO1_SOFT_CAP=1526860800
-ICO1_HARD_CAP=
+PRE_ICO_WALLET_ETH=0x792Cf510b2082c3287C80ba3bb1616D13d2525E3
+PRE_ICO_WALLET_BTC=btcbtc2
 
-ICO1_TOKEN_PRICE=
-ICO1_MIN_PAY=
+PRE_ICO_BONUS1=10               # %
+PRE_ICO_BONUS2=15               # %
+PRE_ICO_BONUS3=20               # %
+PRE_ICO_BONUS4=25               # %
 
-ICO1_WALLET_ETH=
-ICO1_WALLET_BTC=
+# ----------------------------------------------------
 
-ICO1_THRESHOLD1=
-ICO1_BONUS1=
-ICO1_THRESHOLD2=
-ICO1_BONUS2=
-ICO1_THRESHOLD3=
-ICO1_BONUS3=
-ICO1_THRESHOLD4=
-ICO1_BONUS4
-//----------------------------------------------------
-ICO2_START=
-ICO2_END=
+ICO1_START=1530921600           # Wednesday, 4 July 2018 г., 00:00:00
+ICO1_END=1532563199             # Wednesday, 25 July 2018 г., 23:59:59
 
-ICO2_SOFT_CAP=1526860800
-ICO2_HARD_CAP=
+ICO1_SOFT_CAP=1230              # ETH
+ICO1_HARD_CAP=1850              # ETH
 
-ICO2_TOKEN_PRICE=
-ICO2_MIN_PAY=
+ICO1_TOKEN_PRICE=8200           # LDX
+ICO1_MIN_PAY=0.05               # ETH
 
-ICO2_WALLET_ETH=
-ICO2_WALLET_BTC=
+ICO1_WALLET_ETH=0x2382Caf2cc1122b1f13EB10155c5C7c69b88975f
+ICO1_WALLET_BTC=btcbtc3
 
-ICO2_THRESHOLD1=
-ICO2_BONUS1=
-ICO2_THRESHOLD2=
-ICO2_BONUS2=
-ICO2_THRESHOLD3=
-ICO2_BONUS3=
-ICO2_THRESHOLD4=
-ICO2_BONUS4
-//----------------------------------------------------
-ICO3_START=
-ICO3_END=
+# ----------------------------------------------------
 
-ICO3_SOFT_CAP=1526860800
-ICO3_HARD_CAP=
+ICO2_START=1532563200           # Thursday, 26 July 2018 г., 00:00:00
+ICO2_END=1534463999             # Thursday, 16 August 2018 г., 23:59:59
 
-ICO3_TOKEN_PRICE=
-ICO3_MIN_PAY=
+ICO2_SOFT_CAP=1850              # ETH
+ICO2_HARD_CAP=4620              # ETH
 
-ICO3_WALLET_ETH=
-ICO3_WALLET_BTC=
+ICO2_TOKEN_PRICE=4333           # LDX
+ICO2_MIN_PAY=0.05               # ETH
 
-ICO3_THRESHOLD1=
-ICO3_BONUS1=
-ICO3_THRESHOLD2=
-ICO3_BONUS2=
-ICO3_THRESHOLD3=
-ICO3_BONUS3=
-ICO3_THRESHOLD4=
-ICO3_BONUS4
-//----------------------------------------------------
+ICO2_WALLET_ETH=0x57B1fDfE53756e71b1388EcE6cB7C045185BC71C
+ICO2_WALLET_BTC=btcbtc2
+
+# ----------------------------------------------------
+
+ICO3_START=1534464000           # Friday, 17 August 2018 г., 00:00:00
+ICO3_END=1536364799             # Friday, 7 September 2018 г., 23:59:59
+
+ICO3_SOFT_CAP=4620              # ETH
+ICO3_HARD_CAP=10700             # ETH
+
+ICO3_TOKEN_PRICE=2000           # LDX
+ICO3_MIN_PAY=0.05               # ETH
+
+ICO3_WALLET_ETH=0xA9764d8eb302d6a3D363104B94C657849273D5CE
+ICO3_WALLET_BTC=btcbtc2
+
+# -------------------all ico stages bonus -------------
+
+ICO_BONUS1=5                    # %
+ICO_BONUS2=10                   # %
+ICO_BONUS3=15                   # %
+ICO_BONUS4=20                   # %
+
+# ----------------------------------------------------
 
 #----------------IPS------------------------
 
