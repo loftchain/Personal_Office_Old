7786416c2474aa6d902ec4dbc8f92613ccc48b4e
Nikita <Nkmelnikov@hotmail.com>
2018-05-16 12:40:30 +0300

    favicon refresh

--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -10,6 +10,6 @@
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet">
 <link href="{{ asset('css/app.css?v='.env('VERSION')) }}" rel="stylesheet">
-<link rel="icon" type="image/png" href="{{ asset('img/pao.png') }}">
+<link rel="icon" type="image/png" href="https://leadrex.io/img/favicon.ico">
 
 <script src='https://www.google.com/recaptcha/api.js'></script>
