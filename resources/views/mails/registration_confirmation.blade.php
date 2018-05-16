eca7ae98aac1e583ac4283453c199f1c8e9cdf38
Nikita <Nkmelnikov@hotmail.com>
2018-05-16 23:25:14 +0300

    email confirmation restyling

--- resources/views/mails/registration_confirmation.blade.php
+++ resources/views/mails/registration_confirmation.blade.php
@@ -1,6 +1,58 @@
-<div class="reg-conf">
-    <h2>Welcome to Loftchain</h2>
-    You account has been successfully confirmed, now you are more than welcome to invest us all your money.
-    cheers. =)
-    <a href="{{ env('APP_URL') }}">Start investing</a>
-</div>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&amp;subset=cyrillic" rel="stylesheet">
+  <title></title>
+  <style type="text/css">
+    @media only screen and (max-width: 480px){
+      #templateColumns{
+        width:100% !important;
+      }
+
+      .templateColumnContainer{
+        display:block !important;
+        width:100% !important;
+      }
+
+      .columnImage{
+        height:auto !important;
+        max-width:480px !important;
+        width:100% !important;
+      }
+
+      .rightColumnContent{
+        font-size:16px !important;
+        line-height:125% !important;
+      }
+
+      .c-btn{
+        height: 25px;
+        width: 100px;
+        background: #440b3f;
+        color: #A3A3A3;
+      }
+    }
+  </style>
+</head>
+<body style="margin:0; padding:0; color: #A3A3A3; font-family: 'Roboto', sans-serif; background-color:#041123">
+<center>
+  <br><br>
+  <table style="margin:20px 0; padding:20px 0;" width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#041123" id="templateColumns">
+    <tr>
+      <td align="center" valign="top" width="50%" class="templateColumnContainer">
+          <img src="https://leadrex.io/img/leadrex-logo.png" width="280" style="max-width:280px;" class="columnImage">
+          <h1 style="color: #A3A3A3;">Добро пожаловать в LeadRex</h1>
+          <p style="color: #A3A3A3;">Ваш аккаунт был успешно подтверждён</p>
+          <a style="text-decoration: none; padding: 5px 15px; cursor: pointer; border-radius: 5px; background: #440b3f; color: #A3A3A3;" href="{{ env('APP_URL') }}">
+            Начать инвестирование
+          </a>
+      </td>
+    </tr>
+  </table>
+  <br><br>
+</center>
+</body>
+</html>
