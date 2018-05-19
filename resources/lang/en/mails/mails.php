e734248278ff1aab51f036656a3219a1719bdf16
Nikita <Nkmelnikov@hotmail.com>
2018-05-19 17:51:21 +0300

    return mail fix

--- resources/lang/en/mails/mails.php
+++ resources/lang/en/mails/mails.php
@@ -5,4 +5,6 @@ return [
 	'welcome' => 'Welcome to your investor`s personal cabinet',
 	'confirmed' => 'Your account was successfully confirmed by the administration',
 	'startInvesting' => 'Start investing',
+	'returnToKyc_btn' => 'Pass KYC form again',
+	'returnToKyc_txt' => 'Administration of the site, returned you to the step of passing KYC. Perhaps, there were some problems during the uploading of files. Either the Administration found your information insufficient.',
 ];
--- resources/views/mails/return_to_step2.blade.php
+++ resources/views/mails/return_to_step2.blade.php
@@ -1,5 +1,46 @@
-<div class="reg-conf">
-    <h2>Welcome to Loftchain</h2>
-    Unfortunately, our moderation service has rejected your KYC. You where returned to KYC-step. Please try again.
-    <a href="{{ env('APP_URL') }}">Fill KYC fields</a>
-</div>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&amp;subset=cyrillic" rel="stylesheet">
+  <title></title>
+  <style type="text/css">
+    @media only screen and (max-width: 480px){
+      #templateColumns{
+        width:100% !important;
+      }
+
+      .templateColumnContainer{
+        display:block !important;
+        width:100% !important;
+      }
+
+      .columnImage{
+        height:auto !important;
+        max-width:480px !important;
+        width:100% !important;
+      }
+    }
+  </style>
+</head>
+<body style="margin:0; padding:0; color: #A3A3A3; font-family: 'Roboto', sans-serif; background-color:#041123">
+<center>
+  <br><br>
+  <table style="margin:20px 0; padding:20px 0;" width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#041123" id="templateColumns">
+    <tr>
+      <td align="center" valign="top" width="50%" class="templateColumnContainer">
+        <img src="https://leadrex.io/img/leadrex-logo.png" width="280" style="max-width:280px;" class="columnImage">
+        <p style="color: #A3A3A3;">@lang('mails/mails.returnToKyc_txt')</p>
+        <a style="text-decoration: none; padding: 5px 15px; cursor: pointer; border-radius: 5px; background: #440b3f; color: #A3A3A3;" href="{{ env('APP_URL') }}">
+          @lang('mails/mails.returnToKyc_btn')
+        </a>
+      </td>
+    </tr>
+  </table>
+  <br><br>
+</center>
+</body>
+</html>
+
