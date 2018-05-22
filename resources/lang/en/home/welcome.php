a1fa6dfb8793cd79747f727c543288a08123dec4
Nikita <Nkmelnikov@hotmail.com>
2018-05-22 09:36:11 +0300

    bug fixing

--- resources/lang/en/home/welcome.php
+++ resources/lang/en/home/welcome.php
@@ -4,4 +4,6 @@ return [
 	'signUp_a' => 'Registration ',
 	'signIn_a' => 'Log in',
 	'forgotPwd_a' => 'Forgot password?',
+	'uLoggedIn_js' => 'You have successfully logged in',
+	'pwdHasChanged_js' => 'Your password has been changed',
 ];
--- resources/views/_js/js_loader_cloak.blade.php
+++ resources/views/_js/js_loader_cloak.blade.php
@@ -2,12 +2,12 @@
     $(window).on("load", function () {
         $('.cloak').fadeOut("slow");
         if (localStorage.getItem('success_login') !== null) {
-            v.showNotification('Вы успешно вошли в свой аккаунт', 'success');
+            v.showNotification('{{ __('home/welcome.uLoggedIn_js') }}', 'success');
             localStorage.removeItem('success_login')
         }
 
         if (localStorage.getItem('success_reset_pwd') !== null) {
-            v.showNotification('Пароль был успешно изменён.', 'success');
+            v.showNotification('{{ __('home/welcome.pwdHasChanged_js') }}', 'success');
             localStorage.removeItem('success_reset_pwd')
         }
     });
