bd85b72325ce02147ce03e1bcea8049c8286ef63
Nikita <Nkmelnikov@hotmail.com>
2018-06-30 16:54:31 +0300

    new env.ex

--- .env.ex
+++ .env.ex
@@ -12,16 +12,6 @@ DB_DATABASE=pao
 DB_USERNAME=root
 DB_PASSWORD=
 
-BROADCAST_DRIVER=log
-CACHE_DRIVER=file
-SESSION_DRIVER=file
-SESSION_LIFETIME=120
-QUEUE_DRIVER=sync
-
-REDIS_HOST=127.0.0.1
-REDIS_PASSWORD=null
-REDIS_PORT=6379
-
 MAIL_DRIVER=smtp
 MAIL_HOST=smtp.gmail.com
 MAIL_PORT=465
@@ -29,11 +19,18 @@ MAIL_USERNAME=support@loftchain.io
 MAIL_PASSWORD=CdFGAGb5Z4
 MAIL_ENCRYPTION=SSL
 
+
+MAILCHIMP_LIST_ID=184e5d52d7
+MAILCHIMP_API_KEY=d3f58cfc0b7b89981bf77b6820d29985-us18
+MAILCHIMP_FROM_NAME=LeadRex-Support
+MAILCHIMP_FROM_EMAIL=support@loftchain.io
+
+
 RE_CAP_SITE=6LfZykgUAAAAAIRucyy-jq7fNhg3bCB9Oa4GN3k5
 RE_CAP_SECRET=6LfZykgUAAAAAEjsOujIvo0SkVXXD6HKv6WbDE8i
 
-HOME_WALLET_ETH=0xcDa342715528b24eb5E840B847e83900B71dC0F1
-HOME_WALLET_BTC=1CAyLcES1tNuatRhnL1ooPViZ32vF5KQ4A
+HOME_WALLET_ETH=0xb83b1Ade4Be80d7E7559135766Fe2BDc774B35Ca
+HOME_WALLET_BTC=3DmBKSmxsrHLDhBmDmrqRhYVu54TqhB74Q
 
 TOKEN_NAME=LDX
 
@@ -47,7 +44,7 @@ MIN_PAY_RUB=267583
 
 VERSION=0.13
 
-UPLOAD_PATH=C:\OSPanel\domains\pao.loc\public\uploads\
+UPLOAD_PATH=C:\OSPanel\domains\pao.loc\storage\app\uploads\
 
 # ----------------------------------------------------
 
@@ -58,17 +55,14 @@ BONUS_THRESHOLD4=15
 
 # ----------------------------------------------------
 
-PRE_SALE_START=1526860800       # Monday, May 21, 2018 00:00:00
-PRE_SALE_END=1528761599         # Monday, June 11, 2018 23:59:59
+PRE_SALE_START=1526896800       # Monday, May 21, 2018 10:00:00
+PRE_SALE_END=1530230399         # Thursday, June 28, 2018 11:59:59 PM
 
 PRE_SALE_SOFT_CAP=250           # ETH
 PRE_SALE_HARD_CAP=770           # ETH
 
 PRE_SALE_TOKEN_PRICE=16200      # LDX
 PRE_SALE_MIN_PAY=0.1            # ETH
-
-PRE_SALE_WALLET_ETH=0x950D69e56F4dFE84D0f590E0f9F1BdC6d60A46A9
-PRE_SALE_WALLET_BTC=btcbtc1
 
 PRE_SALE_BONUS1=15              # %
 PRE_SALE_BONUS2=20              # %
@@ -77,17 +71,14 @@ PRE_SALE_BONUS4=30              # %
 
 # ----------------------------------------------------
 
-PRE_ICO_START=1528761600        # Tuesday, 12 June 2018 г., 00:00:00
-PRE_ICO_END=1530662399          # Tuesday, 3 July 2018 г., 23:59:59
+PRE_ICO_START=1530316800        # Saturday, June 30, 2018 12:00:00 AM
+PRE_ICO_END=1533340799          # Friday, August 3, 2018 11:59:59 PM
 
 PRE_ICO_SOFT_CAP=770            # ETH
 PRE_ICO_HARD_CAP=1230           # ETH
 
 PRE_ICO_TOKEN_PRICE=13000       # LDX
 PRE_ICO_MIN_PAY=0.1             # ETH
-
-PRE_ICO_WALLET_ETH=0x792Cf510b2082c3287C80ba3bb1616D13d2525E3
-PRE_ICO_WALLET_BTC=btcbtc2
 
 PRE_ICO_BONUS1=10               # %
 PRE_ICO_BONUS2=15               # %
@@ -96,8 +87,8 @@ PRE_ICO_BONUS4=25               # %
 
 # ----------------------------------------------------
 
-ICO1_START=1530921600           # Wednesday, 4 July 2018 г., 00:00:00
-ICO1_END=1532563199             # Wednesday, 25 July 2018 г., 23:59:59
+ICO1_START=1533340800           # Saturday, August 4, 2018 12:00:00 AM
+ICO1_END=1536191999             # Wednesday, September 5, 2018 11:59:59 PM
 
 ICO1_SOFT_CAP=1230              # ETH
 ICO1_HARD_CAP=1850              # ETH
@@ -105,13 +96,10 @@ ICO1_HARD_CAP=1850              # ETH
 ICO1_TOKEN_PRICE=8200           # LDX
 ICO1_MIN_PAY=0.05               # ETH
 
-ICO1_WALLET_ETH=0x2382Caf2cc1122b1f13EB10155c5C7c69b88975f
-ICO1_WALLET_BTC=btcbtc3
-
 # ----------------------------------------------------
 
-ICO2_START=1532563200           # Thursday, 26 July 2018 г., 00:00:00
-ICO2_END=1534463999             # Thursday, 16 August 2018 г., 23:59:59
+ICO2_START=1536192000           # Thursday, September 6, 2018 12:00:00 AM
+ICO2_END=1538956799             # Sunday, October 7, 2018 11:59:59 PM
 
 ICO2_SOFT_CAP=1850              # ETH
 ICO2_HARD_CAP=4620              # ETH
@@ -119,22 +107,16 @@ ICO2_HARD_CAP=4620              # ETH
 ICO2_TOKEN_PRICE=4333           # LDX
 ICO2_MIN_PAY=0.05               # ETH
 
-ICO2_WALLET_ETH=0x57B1fDfE53756e71b1388EcE6cB7C045185BC71C
-ICO2_WALLET_BTC=btcbtc2
-
 # ----------------------------------------------------
 
-ICO3_START=1534464000           # Friday, 17 August 2018 г., 00:00:00
-ICO3_END=1536364799             # Friday, 7 September 2018 г., 23:59:59
+ICO3_START=1541635200           # Thursday, November 8, 2018 12:00:00 AM
+ICO3_END=1544399999             # Sunday, December 9, 2018 11:59:59 PM
 
 ICO3_SOFT_CAP=4620              # ETH
 ICO3_HARD_CAP=10700             # ETH
 
 ICO3_TOKEN_PRICE=2000           # LDX
 ICO3_MIN_PAY=0.05               # ETH
-
-ICO3_WALLET_ETH=0xA9764d8eb302d6a3D363104B94C657849273D5CE
-ICO3_WALLET_BTC=btcbtc2
 
 # -------------------all ico stages bonus -------------
 
@@ -145,12 +127,12 @@ ICO_BONUS4=20                   # %
 
 # ----------------------------------------------------
 
-#----------------IPS------------------------
+#----------------IPS----------------------------------
 
-SELF_API_URL=http://127.0.0.1:8001/
+SELF_API_URL=http://api.loftchain.io/
 OWNER_ID=2
 OWNER_EMAIL=info@leadrex.io
+
 INVESTED_IN_USD=15000
-
-npm install -g vue-generate-component
-
+INVESTED_IN_ETH=770
+INVESTED_IN_BTC=0
