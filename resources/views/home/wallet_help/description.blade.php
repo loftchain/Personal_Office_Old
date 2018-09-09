0cca157d8a62b4d2a49946ed37ac2f3dae398673
Nikita <Nkmelnikov@hotmail.com>
2018-09-09 15:26:05 +0300

    remastering #2

--- resources/views/home/wallet_help/description.blade.php
+++ resources/views/home/wallet_help/description.blade.php
@@ -1,20 +1,16 @@
 <?php $stageInfo = app('\App\Services\BonusService')->getStageInfo() ?>
 
 <div class="x-wallets__text">
-    <ul>
-        <li>@lang('home/wallet.tnxForParticipate_li')</li>
-        @if($currency == 'ETH')
-          <li>@lang('home/wallet.setGas_li') 199 000</li>
-        @endif
-        <li>@lang('home/wallet.minimumPayment_li') {{ $stageInfo['minPayment'] }} ETH</li>
-    </ul>
-    <p class="the-one-wallet"> @lang('home/wallet.singleWallet_p') {{ $currency }}: </p>
-    <div class="pay-to-us-wallet">
-        <img
-                src="{{ asset('img/'. env('HOME_WALLET_'.$currency) .'.gif') }}"
-                alt="QR"
-                class="QR-code">
-        <span class="wallet-name">{{ env('HOME_WALLET_'.$currency) }}</span>
-        <img data-currency="{{ $currency }}" class="w-copy-click w-copy-click-{{ $currency }}" src="{{ asset('img/wallet-copy.png') }}" alt="copy">
-    </div>
+  <ul>
+      <li>@lang('home/wallet.tnxForParticipate_li')</li>
+      @if($currency == 'ETH')
+        <li>@lang('home/wallet.setGas_li') 199 000</li>
+      @endif
+      <li>@lang('home/wallet.minimumPayment_li') {{ $stageInfo['minPayment'] }} ETH</li>
+  </ul>
+  <a type="button" class="x-wallets__text_unified-wallet-button" data-toggle="modal" data-target="#m-ch-email">@lang('home/wallet.singleWallet_p') {{ $currency }}</a>
 </div>
+
+@include('modals.ETH_customers_wallet')
+@include('modals.BTC_customers_wallet')
+
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,6 @@
 <footer>
-    <img src="{{asset('img/foot-wave.png')}}" alt="">
+  <img src="{{asset('img/foot-wave.png')}}" alt="">
+  <div class="developers">
+    <span>Developed by <a href="https://loftchain.io">LoftChain</a> team</span>
+  </div>
 </footer>
