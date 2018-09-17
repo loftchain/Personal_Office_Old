02150725ef738e49e142505ca99fab05d92e8d33
Nikita <Nkmelnikov@hotmail.com>
2018-09-17 14:42:55 +0300

    drift implementation

--- resources/views/_js/js_jivochat.blade.php
+++ resources/views/_js/js_jivochat.blade.php
@@ -1,14 +1,29 @@
-<!-- Chatra {literal} -->
+<!-- drift {literal} -->
 <script>
-    (function(d, w, c) {
-        w.ChatraID = 'ruvZyi6o9NCnFy6vx';
-        var s = d.createElement('script');
-        w[c] = w[c] || function() {
-            (w[c].q = w[c].q || []).push(arguments);
-        };
-        s.async = true;
-        s.src = 'https://call.chatra.io/chatra.js';
-        if (d.head) d.head.appendChild(s);
-    })(document, window, 'Chatra');
+    "use strict";
+
+    !function() {
+        var t = window.driftt = window.drift = window.driftt || [];
+        if (!t.init) {
+            if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+            t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+                t.factory = function(e) {
+                    return function() {
+                        var n = Array.prototype.slice.call(arguments);
+                        return n.unshift(e), t.push(n), t;
+                    };
+                }, t.methods.forEach(function(e) {
+                t[e] = t.factory(e);
+            }), t.load = function(t) {
+                var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+                o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+                var i = document.getElementsByTagName("script")[0];
+                i.parentNode.insertBefore(o, i);
+            };
+        }
+    }();
+    drift.SNIPPET_VERSION = '0.3.1';
+    drift.load('ixuv5prv8sgt');
 </script>
-<!-- /Chatra {/literal} -->
+
+<!-- /drift {/literal} -->
