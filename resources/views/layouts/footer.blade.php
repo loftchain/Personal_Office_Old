8f67d9b6131d59f60705a8fce0d80d0a2048fc5c
Nikita <Nkmelnikov@hotmail.com>
2018-10-12 11:41:55 +0300

    target _blank

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,10 +2,10 @@
   <img src="{{ asset('img/foot-wave.png') }}" alt="">
   <div class="developers">
     <p class="developers__kyc">{!! trans('layouts/footer.KYC_p') !!}</p>
-    <p>{!! trans('layouts/footer.tgg_p') !!} <a href="https://t.me/OpenPackagingNetworkOFFICIAL">{!! trans('layouts/footer.here_a') !!}</a></p>
-    <p>{!! trans('layouts/footer.emergency_p') !!} <a href="https://t.me/joinchat/CAt3yQ_nhtlZlmNjdiX0ow">{!! trans('layouts/footer.here_a') !!}</a></p>
+    <p>{!! trans('layouts/footer.tgg_p') !!} <a target="_blank" href="https://t.me/OpenPackagingNetworkOFFICIAL">{!! trans('layouts/footer.here_a') !!}</a></p>
+    <p>{!! trans('layouts/footer.emergency_p') !!} <a target="_blank" href="https://t.me/joinchat/CAt3yQ_nhtlZlmNjdiX0ow">{!! trans('layouts/footer.here_a') !!}</a></p>
     <br>
     <p>{!! trans('layouts/footer.madein_p') !!}</p>
-    <p>{!! trans('layouts/footer.developed_span') !!} <a href="https://loftchain.io">LoftChain</a></p>
+    <p>{!! trans('layouts/footer.developed_span') !!} <a target="_blank" href="https://loftchain.io">LoftChain</a></p>
   </div>
 </footer>
